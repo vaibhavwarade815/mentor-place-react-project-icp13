b861407ab74d91c325e12917d8b8a43c353b0f6d
akshadasanap7 <akshadasanap297@gmail.com>
2026-04-23 18:58:05 +0530

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import ResumeUpload from "./pages/ResumeUpload";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/upload" element={<ResumeUpload />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Card from "../components/Card";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import { useNavigate } from "react-router-dom";
 import "../styles/Dashboard.css";
 
@@ -17,33 +19,37 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="dashboard">
-      <h1 className="dashboard-title">Welcome back 👋</h1>
+    <>
+      <Navbar />
 
-      {/* Cards Section */}
-      <div className="card-container">
-        <Card title="Resume Score" value={`${score}%`} type="score" />
-        <Card title="Progress" value={`${score}%`} type="progress" />
-        <Card title="Status" value={getStatus()} type="status" />
+      <div className="dashboard">
+        <h1 className="dashboard-title">Welcome back 👋</h1>
+
+        <div className="card-container">
+          <Card title="Resume Score" value={`${score}%`} type="score" />
+          <Card title="Progress" value={`${score}%`} type="progress" />
+          <Card title="Status" value={getStatus()} type="status" />
+        </div>
+
+        <div className="dashboard-buttons">
+          <button
+            className="btn btn-blue"
+            onClick={() => navigate("/upload")}
+          >
+            Upload Resume
+          </button>
+
+          <button
+            className="btn btn-purple"
+            onClick={updateScore}
+          >
+            Refresh Score
+          </button>
+        </div>
       </div>
 
-      {/* Buttons */}
-      <div className="dashboard-buttons">
-        <button
-          className="btn btn-blue"
-          onClick={() => navigate("/upload")}
-        >
-          Upload Resume
-        </button>
-
-        <button
-          className="btn btn-purple"
-          onClick={updateScore}
-        >
-          Refresh Score
-        </button>
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
