49d5a43fcb74558874fd657ef567f6ec1939c883
Dipti17Bhawar <diptibhawar@gmail.com>
2026-04-26 13:03:57 +0530

    Import css in ResumeAnalysis page

--- src/views/ResumeAnalysis/ResumeAnalysis.jsx
+++ src/views/ResumeAnalysis/ResumeAnalysis.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import "../styles/ResumeAnalysis.css";
+import "./ResumeAnalysis.css";
 
 const ResumeAnalysis = () => {
   const location = useLocation();
